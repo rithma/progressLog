cbd1754e16419a6bfdba8af9b7e978b992f662dd
rithma <rithers@gmail.com>
2016-06-06 16:38:13 -0700

    Create addTwoNums_Mine.c

---
+++ addTwoNums_Mine.c
@@ -0,0 +1,87 @@
+ /***************************************************
+ * Recipe:   addTwoNums_c3v0
+ * File:     addTwoNums.c 
+ * Purpose:  Adds numbers using terminal I/O                        
+ ****************************************************
+ *										  			                                
+ * Modification History										                    
+ * 26.02.14 Created
+ * 15.12.15 Updated uVision5.17 & DFP2.6.0   
+ * 																							              
+ * Dr. Mark Fisher, CMP, UEA, Norwich, UK.                                                         
+ ****************************************************/
+
+#include "stm32F4xx_hal.h"
+#include <stdio.h>
+#include "Serial.h" 
+#include "cmsis_os.h"
+
+#ifdef __RTX
+extern uint32_t os_time;
+
+uint32_t HAL_GetTick(void) {
+  return os_time; 
+}
+#endif
+
+/*--------------------------------------------------
+  System Clock Configuration
+ *--------------------------------------------------*/
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the
+     device is clocked below the maximum system frequency (see datasheet). */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 |
+                                RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
+
+/*
+ * main
+ *******/
+int main (void) {
+	
+	int input;
+//	//uint8_t num1, num2, res;	
+	int num1, num2, res;
+
+  HAL_Init ();   /* Init Hardware Abstraction Layer */
+  SystemClock_Config ();           /* Config Clocks */
+
+  SER_Init();
+  
+  for (;;) {                        /* Loop forever */  
+		printf("Enter First Number: ");
+	  scanf("%d", &input);
+		num1 = (int) input;
+		printf("Enter Second Number: ");
+	  scanf("%d", &input);
+		num2 = (int) input;
+  	res = num1 + num2;
+		printf("Result = %d \n", res);
+  }
+}
